92804f0867265c5638074f15b7521762db4ff2bc
AromDaMassa <coronil.arom@escola.pr.gov.br>
2024-09-18 07:45:05 -0300

    .

--- js/script.js
+++ js/script.js
@@ -7,10 +7,24 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const BotaoJogarNovamente= document.querySelector(".novamente-btn");
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(''.telainicial);
 
 let atual = 0; 
 let perguntaAtual;
 let historiaFinal = "";
+
+botaoIniciar.addEvenetListener('click,iniciaJogo');
+
+function inicaJogo() {
+    atual = 0;
+    historiaFinal = "";
+    telaInicial,style.display = none;
+    caixaPerguntas.classList.remove('mostrar');
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
+}
 
 function mostraPergunta() {
     if(atual >= perguntas.length){
@@ -52,4 +66,9 @@ mostraergunta();
 
 }
 
-mostraPergunta();
+function substituiNome () {
+    for(const pergunta of perguntas){
+        pergunta.enunciado = pergunta.enunciado.replace(/você/g. nome)
+    }
+}
+substituiNome();
